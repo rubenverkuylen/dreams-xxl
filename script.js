07d4d624bc72e15aa0c2d7d9b5687593ed51e327
rubenverkuylen <info@rubenverkuylen.nl>
2023-04-03 10:47:45 +0200

    UX: scroll, resizing

--- script.js
+++ script.js
@@ -16,15 +16,28 @@ function hideContent() {
 // 2. open work content
 function clickHeader(elem) {
   const sibling = elem.nextElementSibling;
-  // hide all
   if (!sibling.classList.contains("hide")) {
+    elem.classList.remove("extra-margin");
     sibling.classList.add("hide");
   } else {
     workContent.forEach((el) => {
       el.classList.add("hide");
+      el.previousElementSibling.classList.remove("extra-margin");
     });
+    if (window.outerWidth <= 800) {
+      elem.classList.add("extra-margin");
+    }
     sibling.classList.remove("hide");
+    if (window.outerWidth <= 800) {
+      scrollToOffset(elem);
+    }
   }
+}
+
+function scrollToOffset(elem) {
+  const yOffset = -50;
+  const y = elem.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  window.scrollTo({ top: y, behavior: "smooth" });
 }
 
 // 3. open menu
@@ -51,6 +64,29 @@ function topFunction() {
 
 openModal.addEventListener("click", openMenu);
 closeModal.addEventListener("click", closeMenu);
+
+// 4. reset on browser resize
+window.addEventListener("resize", (e) => {
+  const width = e.target.outerWidth;
+  if (width >= 800) {
+    showAbout();
+  } else {
+    hideAbout();
+  }
+});
+
+function hideAbout() {
+  about.classList.add("hide");
+}
+
+function showAbout() {
+  about.classList.remove("hide");
+  works.classList.remove("hide");
+}
+
+// 1. check if broweser gets resized
+// 2. if browser > 800px -> remove .hide from about
+// 3. if browser < 800px -> add .hide to about
 
 // run on load
 (function () {
